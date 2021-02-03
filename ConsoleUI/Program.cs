fd9e93f73cd17e7b0beda6c2015bcf9cdd36d532
KucukdogruMerve <33333935+Kucukdogru@users.noreply.github.com>
2021-02-03 23:48:22 +0300

    2 Şubat 2021 Ders Sonu

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,16 +1,19 @@
 ﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
 using DataAccess.Concrete.InMemory;
 using System;
 
 namespace ConsoleUI
 {
+    // SOLID
+    // O-> OPEN CLOSED PRINCIPLE
     class Program
     {
         static void Main(string[] args)
         {
-            ProductManager productManager = new ProductManager(new InMemoryProductDal());
+            ProductManager productManager = new ProductManager(new EfProductDal());
 
-            foreach (var product in productManager.GetAll())
+            foreach (var product in productManager.GetByUnitPrice(50, 200)) 
             {
                 Console.WriteLine(product.ProductName);
             }
---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,27 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    // GENERIC CONSTRAINT
+    // class : referans tip
+    // IEntity : IEntity olabilir veya IEntity implemente eden bir nesne olabilir
+    // new() : new'lenebilir olmalıdır
+
+    public interface IEntityRepository<T> where T:class, IEntity, new()
+    {
+        // Tek bir kredinin detayına gitmek için 
+        List<T> GetAll(Expression<Func<T, bool>> filter=null);
+
+        T Get(Expression<Func<T, bool>> filter);
+
+        void Add(T entity);
+
+        void Update(T entity);
+
+        void Delete(T entity);
+    }
+}
